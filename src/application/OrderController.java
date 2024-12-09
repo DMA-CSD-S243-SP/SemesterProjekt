68781b3818f6248463d79cbf576f75eae6813632
LumiUCN <10632106@ucn.dk>
2024-12-09 11:15:20 +0100

    Implemented Order Controller
    Documentation needed

--- src/application/OrderController.java
+++ src/application/OrderController.java
@@ -1,8 +1,41 @@
 package application;
 
+import model.Customer;
+import model.Order;
+import model.OrderContainer;
+import model.OrderLine;
+import model.Product;
 
 public class OrderController
 {
+	private Order o;
 	
 	
+	public OrderController() {
+		
+	}
+	
+	public void makeNewOrder() {
+		o = new Order();
+		
+	}
+	
+	public Customer addCustomerByPhoneNumber(String phone) {
+		Customer c = new CustomerController().findCustomerByPhone(phone);
+		o.setCustomer(c);
+		return c;
+	}
+	
+	public double enterProduct(String productID, int quantity) {
+		OrderLine ol = new OrderLine();
+		Product p = new ProductController().findProductByID(productID);
+		ol.setProduct(p);
+		ol.setQuantity(quantity);
+		o.addOrderLine(ol);
+		return o.getTotal();
+	}
+	
+	public void endSale() {
+		OrderContainer.addOrder(o);
+	}
 }
