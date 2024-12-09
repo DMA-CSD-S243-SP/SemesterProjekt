8869f4e8be3c45eb642ae3943d303e4e41a483cb
LumiUCN <10632106@ucn.dk>
2024-12-09 11:13:44 +0100

    Implemented ProductController

--- src/application/ProductController.java
+++ src/application/ProductController.java
@@ -1,8 +1,16 @@
 package application;
 
+import model.Product;
+import model.ProductContainer;
 
 public class ProductController 
 {
-
+	public ProductController() {
+		
+	}
+	
+	public Product findProductByID(String productID) {
+		return ProductContainer.findProductByID(productID);
+	}
 	
 }
