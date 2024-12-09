e446a1911cb3b5492fdca3069b799d254cc60b09
AndersUCN <10632098@ucn.dk>
2024-12-09 09:01:21 +0100

    Update CustomerCategory.java

--- src/model/CustomerCategory.java
+++ src/model/CustomerCategory.java
@@ -3,6 +3,32 @@ package model;
 
 public class CustomerCategory
 {
+	private String name; 
+	private boolean locked;
+	private double discount;
+	private String description;
 	
 	
+	public CustomerCategory(boolean isLocked, double discount, String description) 
+	{
+		locked = isLocked;
+		this.discount = discount;
+		this.description = description;
+	}
+	
+	
+	public boolean isLocked()
+	{
+		return locked;
+	}
+	
+	public double getDiscount()
+	{
+		return discount;
+	}
+	
+	public String getDescription()
+	{
+		return description;
+	}
 }
