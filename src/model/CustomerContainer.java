b7fa4c1ea28b53d609bc76869c5bbecdd49ae410
AndersUCN <10632098@ucn.dk>
2024-12-09 10:16:04 +0100

    Update CustomerContainer.java

--- src/model/CustomerContainer.java
+++ src/model/CustomerContainer.java
@@ -1,8 +1,35 @@
 package model;
 
+import java.util.HashMap;
+import java.util.Map;
 
 public class CustomerContainer
 {
+	private static Customer anonymousCustomer = createAnonymous();
+	private static Map<String, Customer> customerByPhone = new HashMap<>();
+	private static Map<String, CustomerCategory> categoriesByName = new HashMap<>();
 	
 	
+	private CustomerContainer()
+	{
+		
+	}
+	
+	
+	private  static Customer createAnonymous()
+	{
+		return new Customer();
+	}
+	
+	
+	public Customer getAnonymous()
+	{
+		return anonymousCustomer;
+	}
+	
+	
+	public static Customer findCustomerByPhone(String phone)
+	{
+		return customerByPhone.get(phone);
+	}
 }
