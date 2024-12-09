07cf7e012c2c42364f2b54584abc2ae15f143012
Line-B-333 <10187764@ucn.dk>
2024-12-09 17:40:03 +0100

    Update OrderLine
    Added comments to orderLine methods

--- src/model/OrderLine.java
+++ src/model/OrderLine.java
@@ -1,10 +1,10 @@
 package model;
 
 /**
- * Write a description of class RentController here.
+ * Represent an order line containing a product and its quantity.
  *
  * @author Line Bertelsen
- * @version 0.0.1 initial draft version
+ * @version 09/12/2024 - 17.30
  */
 
 
@@ -20,24 +20,44 @@ public class OrderLine
 	}
 		
 	
+	/**
+	 * The setQuantity method sets the quantity for this order line. 
+	 * 
+	 * @param quantity of the product
+	 */
 	public void setQuantity(int quantity)
 	{
 		this.quantity = quantity;
 	}
 	
-
+	
+	/**
+	 * The setProduct method set the product for this order line.
+	 * 
+	 * @param product 
+	 */
 	public void setProduct(Product product)
 	{
 		this.product = product; 
 	}
 	
 	
+	/**
+	 * The getSubTotal method calculates the subtotal based on the products price. 
+	 * 
+	 * @return the subtotal of this order line. 
+	 */
 	public double getSubTotal()
 	{
 		return quantity * product.getPrice();
 	}
 	
 	
+	/**
+	 * The getMinimumSubtotal method calculates the minimum subtotal based on the products minimum price. 
+	 * 
+	 * @return the minimum subtotal for this order line. 
+	 */
 	public double getMinimumSubtotal()
 	{
 		return quantity * product.getMinimumPrice();
