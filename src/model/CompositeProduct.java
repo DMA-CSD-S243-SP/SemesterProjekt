6a9412e8cb328e5179737fa48ee0dc13406b323b
LumiUCN <10632106@ucn.dk>
2024-12-09 09:22:28 +0100

    Implemented CompositeProduct

--- src/model/CompositeProduct.java
+++ src/model/CompositeProduct.java
@@ -1,8 +1,46 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
 
-public class CompositeProduct
+public class CompositeProduct extends Product
 {
+	private List<CompositeLine> compositeLineList;
+	private double discount;
 	
+	/**
+	 * Constructs a composite product.
+	 */
+	public CompositeProduct() {
+		super();
+		compositeLineList = new ArrayList<>();
+	}
+	
+	/**
+	 * Accessor for the total price of a compositeProduct
+	 * @return The total price of the compositeProduct
+	 */
+	@Override
+	public double getPrice() {
+		double sum = 0;
+		for (CompositeLine currentLine : compositeLineList) {
+			Product currentProduct = currentLine.getProduct();
+			sum = sum + (currentProduct.getPrice() * currentLine.getQuantity());
+		}
+		return sum;
+	}
+	/**
+	 * Accessor for the total minimum price of a compositeProduct
+	 * @return The total minimum price of the compositeProduct
+	 */
+	@Override
+	public double getMinimumPrice() {
+		double sum = 0;
+		for (CompositeLine currentLine : compositeLineList) {
+			Product currentProduct = currentLine.getProduct();
+			sum = sum + (currentProduct.getMinimumPrice() * currentLine.getQuantity());
+		}
+		return sum;
+	}
 	
 }
