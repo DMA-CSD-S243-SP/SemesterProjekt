b954fd5203f82472e55f614cdbec6a21c96c2f76
AndersUCN <10632098@ucn.dk>
2024-12-09 11:04:45 +0100

    Lavet ProductContainer

--- src/model/ProductContainer.java
+++ src/model/ProductContainer.java
@@ -1,8 +1,21 @@
 package model;
 
+import java.util.HashMap;
+import java.util.Map;
 
 public class ProductContainer
 {
+	private static Map<String, Product> productByID = new HashMap<>();
 	
 	
+	private ProductContainer()
+	{
+		
+	}
+	
+	
+	public static Product findProductByID(String productID)
+	{
+		return productByID.get(productID);
+	}
 }
