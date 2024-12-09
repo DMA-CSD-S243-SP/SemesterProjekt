fe7fa96d17d682fc80ae8006537866753fd9c9af
Daniel-Heby <10632084@ucn.dk>
2024-12-09 09:24:52 +0100

    Class Customer er lavet
    Har tilføjet variablerne, og metoderne så de stemmer overens med design diagram

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -1,8 +1,30 @@
 package model;
+import java.util.List;
+import java.util.ArrayList;
 
 
-public class Customer
-{
-
+public class Customer {
+	private String name;
+	private String address;
+	private String phone;
+	private String mailAddress;
+	private double discount;
+	private List<Order> orderList;
+	private CustomerCategory category;
 	
+	
+public Customer(String name, String address, String phone, String mailAddress, double discount, List<Order> orderList, CustomerCategory category) {	
+	this.name = name;
+	this.address = address;
+	this.phone = phone;
+	this.mailAddress = mailAddress;
+	this.discount = discount;
+	this.orderList = new ArrayList<>();
+	this.category = category;
+}
+
+
+public double getDiscount() {
+	return discount;
+}
 }
