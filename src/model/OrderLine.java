0062409c316aa450f7bd8c659ace15a0365bf3c7
Line-B-333 <10187764@ucn.dk>
2024-12-09 09:25:48 +0100

    Update Orderline
    En opdatering af orderline - Line

--- src/model/OrderLine.java
+++ src/model/OrderLine.java
@@ -3,6 +3,36 @@ package model;
 
 public class OrderLine
 {
+	private int quantity;
+	private Product product;
 	
 	
+	public OrderLine() 
+	{
+		
+	}
+		
+	
+	public void setQuantity(int quantity)
+	{
+		this.quantity = quantity;
+	}
+	
+
+	public void setP(Product product)
+	{
+		this.product = product; 
+	}
+	
+	
+	public double getSubtotal()
+	{
+		return quantity * product.getPrice();
+	}
+	
+	
+	public double getMinimumSubtotal()
+	{
+		return quantity * product.getMinimumPrice();
+	}
 }
