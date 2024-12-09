4cc3401d0a6d6f850dd5e33d68bc3d5f07128804
Daniel-Heby <10632084@ucn.dk>
2024-12-09 11:08:30 +0100

    CustomerController lavet
    variabler og metode addet og lavet.

--- src/application/CustomerController.java
+++ src/application/CustomerController.java
@@ -1,8 +1,17 @@
 package application;
 
+import model.Customer;
+import model.CustomerContainer;
 
-public class CustomerController 
-{
+
+public class CustomerController {
 	
-
+	
+	public CustomerController() {}
+	
+	
+	public Customer findCustomerByPhone(String phone) {
+		Customer customer = CustomerContainer.findCustomerByPhone(phone);
+		return customer;
+	}
 }
