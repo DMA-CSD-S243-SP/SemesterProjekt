967684d1bfe4a52553a9927d6fc8d3a12acf75e2
ManifestManah <ManifestManah@users.noreply.github.com>
2024-12-12 22:13:45 +0100

    Updated: Readability of SaleUI
    Changed some of the formatting to make the SaleUI class have better readability and align more with the coding standards.

--- src/ui/SaleUI.java
+++ src/ui/SaleUI.java
@@ -22,15 +22,18 @@ public class SaleUI extends Option
 		super(optionText);
 	}
 
+	
 	@Override
 	void runOption()
 	{
 		orderControl = new OrderController();
-		// makeNewOrder
+		
+		// Operation contract: makeNewOrder
 		orderControl.makeNewOrder();
 
-		// addCustomerByPhone(phone)
+		// Operation contract: addCustomerByPhone(phone)
 		boolean isCustomerFound = false;
+		
 		while (isCustomerFound == false)
 		{
 			System.out.println("\n\n" + phoneRequest);
@@ -38,12 +41,15 @@ public class SaleUI extends Option
 			isCustomerFound = tryAddCustomerByPhone(phone);
 		}
 
-		// enterProduct(productID, quantity)
+		
+		// Operation contract: enterProduct(productID, quantity)
 		boolean isDone = false;
+		
 		while (isDone == false)
 		{
 			System.out.println("\n\n" + productAndExitRequest + "[" + exitCode + "]");
 			String productID = InputHandler.getInput();
+			
 			if (productID.equals(exitCode)) // endSale
 			{
 				isDone = true;
@@ -54,13 +60,13 @@ public class SaleUI extends Option
 				int quantity = inputQuantity();
 				tryEnterProduct(productID, quantity);
 			}
-
 		}
 
 		orderControl.endSale();
 		orderControl = null;
 	}
 
+	
 	/**
 	 * Tries to add a customer to the Order, and prints either information about the
 	 * customer, or an error code.
@@ -75,12 +81,15 @@ public class SaleUI extends Option
 			System.out.println("Customer Hash: " + foundCustomer); // Uses Customer.toString(). Returns hashcode,
 																	// should be changed.
 			return true;
-		} catch (NullPointerException exception)
+		}
+		
+		catch (NullPointerException exception)
 		{
 			System.out.println(exception.getMessage());
 			return false;
 		}
 	}
+	
 
 	/**
 	 * Requests input until a valid quantity is given.
@@ -90,29 +99,36 @@ public class SaleUI extends Option
 	private int inputQuantity()
 	{
 		int quantity = 0;
-		while (quantity <= 0) // Retries if quantity hasn't been set yet, or has been set to a non-positive
-								// number
+		
+		while (quantity <= 0) // Retries if quantity hasn't been set yet, or has been set to a non-positive number
 		{
 			System.out.println("\n\nRegister antal");
 			String inputLine = InputHandler.getInput();
+			
 			if (inputLine.equals("")) // Allows quick input of a single product, by hitting enter
 			{
 				quantity = 1;
-			} else
+			}
+			
+			else
 			{
 				try
 				{
 					quantity = Integer.parseInt(inputLine);
-				} catch (NumberFormatException exception)
+				}
+				
+				catch (NumberFormatException exception)
 				{
 					System.out.println(invalidNumber);
 				}
 			}
 
 		}
+		
 		return quantity;
 	}
 
+	
 	/**
 	 * Tries to enter a product with it's quantity, prints total if succesful, and
 	 * an error message if not
@@ -126,7 +142,9 @@ public class SaleUI extends Option
 		{
 			double total = orderControl.enterProduct(productID, quantity);
 			System.out.println("\n\nTotal: " + total);
-		} catch (NullPointerException exception)
+		}
+		
+		catch (NullPointerException exception)
 		{
 			System.out.println(exception.getMessage());
 		}
