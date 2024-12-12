17097c64f3f593930340d47cc9f2b2733eb313fd
ManifestManah <ManifestManah@users.noreply.github.com>
2024-12-12 22:38:53 +0100

    Added: Missing Author to CompositeProduct
    Added an author note to the CompositeProduct class, as it had been overlooked.

--- src/model/CompositeProduct.java
+++ src/model/CompositeProduct.java
@@ -6,12 +6,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-
+/**
+ * 
+ *
+ * 
+ * 
+ * 
+ * 
+ * 
+ * @author Lumière Schack & Christoffer Søndergaard
+ * @version 12/12/2024 - 22:38
+ */
 public class CompositeProduct extends Product
 {
 	private List<CompositeLine> compositeLineList;
 	private double discount;
 
+	
 	/**
 	 * Constructs a composite product.
 	 */
@@ -21,6 +32,7 @@ public class CompositeProduct extends Product
 		compositeLineList = new ArrayList<>();
 	}
 
+	
 	/**
 	 * Accessor for the total price of a compositeProduct
 	 * 
@@ -30,14 +42,17 @@ public class CompositeProduct extends Product
 	public double getPrice()
 	{
 		double sum = 0;
+		
 		for (CompositeLine currentLine : compositeLineList)
 		{
 			Product currentProduct = currentLine.getProduct();
 			sum = sum + (currentProduct.getPrice() * currentLine.getQuantity());
 		}
+		
 		return sum;
 	}
 
+	
 	/**
 	 * Accessor for the total minimum price of a compositeProduct
 	 * 
@@ -47,12 +62,13 @@ public class CompositeProduct extends Product
 	public double getMinimumPrice()
 	{
 		double sum = 0;
+		
 		for (CompositeLine currentLine : compositeLineList)
 		{
 			Product currentProduct = currentLine.getProduct();
 			sum = sum + (currentProduct.getMinimumPrice() * currentLine.getQuantity());
 		}
+		
 		return sum;
 	}
-
 }
