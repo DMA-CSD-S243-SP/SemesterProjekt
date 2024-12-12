e3990e8303128597f8f792e4f6e2de4ac3197f52
LumiUCN <10632106@ucn.dk>
2024-12-12 12:22:50 +0100

    Made compositeProduct format compliant

--- src/model/CompositeProduct.java
+++ src/model/CompositeProduct.java
@@ -7,40 +7,48 @@ public class CompositeProduct extends Product
 {
 	private List<CompositeLine> compositeLineList;
 	private double discount;
-	
+
 	/**
 	 * Constructs a composite product.
 	 */
-	public CompositeProduct() {
+	public CompositeProduct()
+	{
 		super();
 		compositeLineList = new ArrayList<>();
 	}
-	
+
 	/**
 	 * Accessor for the total price of a compositeProduct
+	 * 
 	 * @return The total price of the compositeProduct
 	 */
 	@Override
-	public double getPrice() {
+	public double getPrice()
+	{
 		double sum = 0;
-		for (CompositeLine currentLine : compositeLineList) {
+		for (CompositeLine currentLine : compositeLineList)
+		{
 			Product currentProduct = currentLine.getProduct();
 			sum = sum + (currentProduct.getPrice() * currentLine.getQuantity());
 		}
 		return sum;
 	}
+
 	/**
 	 * Accessor for the total minimum price of a compositeProduct
+	 * 
 	 * @return The total minimum price of the compositeProduct
 	 */
 	@Override
-	public double getMinimumPrice() {
+	public double getMinimumPrice()
+	{
 		double sum = 0;
-		for (CompositeLine currentLine : compositeLineList) {
+		for (CompositeLine currentLine : compositeLineList)
+		{
 			Product currentProduct = currentLine.getProduct();
 			sum = sum + (currentProduct.getMinimumPrice() * currentLine.getQuantity());
 		}
 		return sum;
 	}
-	
+
 }
