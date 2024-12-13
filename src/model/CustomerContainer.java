6f79b888dee9da0abfd29f1c258538834a4115fe
ManifestManah <ManifestManah@users.noreply.github.com>
2024-12-13 10:17:30 +0100

    Updated: CustomerContainer Class Documentation
    Updated the documentation of the CustomerContainer class.

--- src/model/CustomerContainer.java
+++ src/model/CustomerContainer.java
@@ -7,15 +7,13 @@ import java.util.Map;
 
 
 /**
- * Represents a Container for customers that includes a hashmap of customer and
- * of Category
+ * Represents a container that 'Customer' objects can be stored within and make them
+ * easily accessible by using the 'Customer' object's unique phone field's value
+ * as the key used for retrieving said 'Customer' objects stored within the collection.
+ * The collection utilizes a HashMap for its collection.
  *
- * The CustomerContainer class is a container which holds both customers and
- * categories.
- * 
- * 
  * @author Anders Have & Christoffer Søndergaard
- * @version 12/12/2024 - 23:06
+ * @version 13/12/2024 - 10:18
  */
 public class CustomerContainer
 {
@@ -23,7 +21,7 @@ public class CustomerContainer
 	private static Map<String, Customer> customerByPhone = new HashMap<>();
 	private static Map<String, CustomerCategory> categoriesByName = new HashMap<>();
 	
-	
+	// Creates a Customer object and 
 	private static Customer anonymousCustomer = createAnonymous();
 
 	
@@ -40,29 +38,39 @@ public class CustomerContainer
 	{
 
 	}
-
+	
 	
 	/**
-	 * creates a new Customer
+	 * Creates and adds a Customer object, to the HashMap's collection as an anonymous customer.
 	 *
-	 * creates and returns a new customer.
-	 *
-	 * @return the new customer that was created
+	 * Creates the 'Customer' object, and adds it to the HashMap's collection with an empty string
+	 * as the associated 'Customer' objects key within the Hashmap, and gives the customer the
+	 * name anonymousCustomer, to make it retrievable in situations where a customer is not to
+	 * be registered in the system e.g. during regular sales.
+	 * 
+	 * @return the 'Customer' stored within the anonymousCustomer field.
 	 */
 	private static Customer createAnonymous()
 	{
+		// Creates a new customer object and stores it within the anonymous variable
 		Customer anonymous = new Customer();
+		
+		// Adds the Customer to the HashMap collection by the name "anonymous". 
+		// An anonymous customer will have an empty string as their phone number as they are anonymous.
+		// The anonymous customer can be found in the system by the employee typing in nothing when assigning a customer to an order.
 		customerByPhone.put("", anonymous);
+		
 		return anonymous;
 	}
 
 	
 	/**
-	 * Retrieves the anonymousCustomer field
+	 * Retrieves the value of the anonymousCustomer field.
 	 *
-	 * Returns the value of the 'anonymousCustomer' field, which is a customer.
-	 *
-	 * @return the anonymousCustomer field
+	 * Returns the 'Customer' object, which is stored within the static
+	 * anonymousCustomer field.
+	 * 
+	 * @return the 'Customer' stored within the anonymousCustomer field.
 	 */
 	public Customer getAnonymous()
 	{
@@ -71,12 +79,12 @@ public class CustomerContainer
 
 	
 	/**
-	 * Retrieves the customer
+	 * Retrieves the Customer object that has a matching phone number.
 	 *
-	 * Returns the value which the key(phone) is associated with from the HashMap
-	 * customerByPhone.
+	 * Returns the 'Customer' object, which is stored within the HashMap collection
+	 * with the specified phone number as their associated key.
 	 *
-	 * @return the value which the key(phone) is associated with.
+	 * @return the 'Customer' which is associated with the key.
 	 */
 	public static Customer findCustomerByPhone(String phone)
 	{
